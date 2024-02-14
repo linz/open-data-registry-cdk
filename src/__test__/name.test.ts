9eacf33dd052225d55121f84add0db09e83c58c7
Victor Engmark <vengmark@linz.govt.nz>
2024-02-15 10:50:38 +1300

    test: Verify elevation-related title case conversions

--- src/__test__/name.test.ts
+++ src/__test__/name.test.ts
@@ -5,11 +5,12 @@ import { titleCase } from '../util/names.js';
 describe('titleCase', () => {
   it('should title case from dash case', () => {
     assert.equal(titleCase('linz-imagery-bucket'), 'LinzImageryBucket');
-    assert.equal(titleCase('linz_imagery_Bucket'), 'LinzImageryBucket');
+    assert.equal(titleCase('linz_elevation_Bucket'), 'LinzElevationBucket');
     assert.equal(titleCase('linz imagery bucket'), 'LinzImageryBucket');
   });
 
   it('should support a mixture of - _ and " "', () => {
     assert.equal(titleCase('linz_imagery bucket-two'), 'LinzImageryBucketTwo');
+    assert.equal(titleCase('linz_elevation bucket-two'), 'LinzElevationBucketTwo');
   });
 });
